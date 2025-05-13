8b79b45a6b0c48795f68a901941072cbdfa972c9
deepa-124 <deepashankargodpatil@mail.com>
2025-05-13 15:36:23 +0530

    add a new repo

---
+++ Person.java/Main1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee emp = new Employee("Dom", "1990-05-06", "202", 70000);
+        emp.displayEmployeeInfo();    
+    }
+}
